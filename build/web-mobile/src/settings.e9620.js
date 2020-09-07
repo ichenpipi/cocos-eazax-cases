685826ba7c0001c09b682fe7ed6ebc2d19cf0798
陈皮皮 <952157129@qq.com>
2020-09-08 01:25:47 +0800

    update

---
+++ build/web-mobile/src/settings.e9620.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"b25de",main:"45ef2"}};
