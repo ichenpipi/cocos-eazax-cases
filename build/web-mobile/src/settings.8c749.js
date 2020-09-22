ed4fb0b49122dca809b73c70e72427361c59770e
陈皮皮 <952157129@qq.com>
2020-09-22 16:38:53 +0800

    update

---
+++ build/web-mobile/src/settings.8c749.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"9d7cc",internal:"b25de",main:"16454"}};
--- build/web-mobile/src/settings.f33ae.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"b25de",main:"9cc38"}};
