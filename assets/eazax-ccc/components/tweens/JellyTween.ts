43b6d75dc7ae5017c530c8ed18770206f65cca89
陈皮皮 <952157129@qq.com>
2020-10-12 18:28:57 +0800

    update

--- assets/eazax-ccc/components/tweens/JellyTween.ts
+++ assets/eazax-ccc/components/tweens/JellyTween.ts
@@ -1,30 +1,42 @@
 const { ccclass, property } = cc._decorator;
 
+/**
+ * 果冻缓动效果
+ * @see JellyTween.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/components/tweens/JellyTween.ts
+ */
 @ccclass
 export default class JellyTween extends cc.Component {
 
     @property({ tooltip: CC_DEV && '频率（回弹次数）' })
-    private frequency: number = 4;
+    public frequency: number = 4;
 
-    @property({ tooltip: CC_DEV && '衰退速度' })
-    private decay: number = 3;
+    @property({ tooltip: CC_DEV && '衰退指数' })
+    public decay: number = 3;
 
-    @property({ tooltip: CC_DEV && '间隔' })
-    private interval: number = 0.5;
+    @property({ tooltip: CC_DEV && '播放间隔' })
+    public interval: number = 0.5;
 
     @property({ tooltip: CC_DEV && '自动播放' })
-    private playOnLoad: boolean = false;
+    public playOnLoad: boolean = false;
 
-    private tween: any = null;
+    private tween: cc.Tween = null;
 
     protected start() {
         if (this.playOnLoad) this.play();
     }
 
-    public play() {
-        const w = this.frequency * Math.PI * 2;
+    /**
+     * 播放
+     * @param repeatTimes 重复次数
+     */
+    public play(repeatTimes?: number) {
+        // 重复次数
+        const times = (repeatTimes != undefined && repeatTimes > 0) ? repeatTimes : 10e8;
+        // 角速度（ω=2nπ）
+        const angularVelocity = this.frequency * Math.PI * 2;
+        // 播放
         this.tween = cc.tween(this.node)
-            .repeatForever(
+            .repeat(times,
                 cc.tween()
                     .to(0.2, { scaleX: 1.1, scaleY: 0.9 }, { easing: 'sineOut' })
                     .to(0.15, { scaleX: 1, scaleY: 1 }, { easing: 'sineOut' })
@@ -33,7 +45,7 @@ export default class JellyTween extends cc.Component {
                         scaleY: {
                             value: 1,
                             progress: (start: number, end: number, current: number, t: number) => {
-                                return end + end * (Math.sin(t * w) / Math.exp(this.decay * t) / w);
+                                return end + (end * (Math.sin(t * angularVelocity) / Math.exp(this.decay * t) / angularVelocity));
                             }
                         }
                     })
@@ -42,9 +54,12 @@ export default class JellyTween extends cc.Component {
             .start();
     }
 
+    /**
+     * 停止
+     */
     public stop() {
-        this.node.stopAction(this.tween);
-        this.node.scale = 1;
+        this.tween && this.tween.stop();
+        this.node.setScale(1);
     }
 
 }
