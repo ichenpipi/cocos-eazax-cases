d7483e4f4207740cb5ba84806267fa0f28b68a5f
陈皮皮 <952157129@qq.com>
2020-10-17 02:29:15 +0800

    update

--- assets/eazax-ccc/core/PopupManager.ts
+++ assets/eazax-ccc/core/PopupManager.ts
@@ -12,6 +12,9 @@ export default class PopupManager {
     /** 节点表 */
     private static nodeMap: Map<string, cc.Node> = new Map<string, cc.Node>();
 
+    /** 模式表 */
+    private static modeMap: Map<string, PopupCacheMode> = new Map<string, PopupCacheMode>();
+
     /** 等待队列 */
     public static get queue() { return this._queue; }
     private static _queue: PopupRequest[] = [];
@@ -30,64 +33,79 @@ export default class PopupManager {
      * 展示弹窗
      * @param path 弹窗预制体相对路径（如：prefabs/popup）
      * @param options 弹窗选项
-     * @param mode 回收模式
+     * @param mode 缓存模式
      * @param priority 是否优先展示
      */
-    public static async show(path: string, options: any = null, mode: PopupRecycleMode = PopupRecycleMode.Temporary, priority: boolean = false): Promise<boolean> {
-        if (this._curPopup) {
-            this.push(path, options, mode, priority);
-            cc.log('[PopupManager]', '弹窗已加入等待队列', this._queue);
-            return false;
-        }
-
+    public static async show(path: string, options: any = null, mode: PopupCacheMode = PopupCacheMode.Temporary, priority: boolean = false): Promise<boolean> {
         return new Promise(async res => {
-            this._curPopup = { path, options, mode };
-
-            let node: cc.Node = null;
-            // let curMode: PopupRecycleMode = null;
-
-            // 先在缓存中查找
-            if (this.prefabMap.has(path)) {
-                // 从预制表中获取
-                const prefab = this.prefabMap.get(path);
-                if (cc.isValid(prefab)) node = cc.instantiate(prefab);
-                else this.prefabMap.delete(path);
-                // curMode = PopupRecycleMode.Temporary;
-            } else if (this.nodeMap.has(path)) {
-                // 从节点表中获取
-                node = this.nodeMap.get(path);
-                if (!cc.isValid(node)) this.nodeMap.delete(path);
-                // curMode = PopupRecycleMode.Frequent;
+            // 当前已有弹窗在展示中则加入等待队列
+            if (this._curPopup) {
+                this.push(path, options, mode, priority);
+                cc.log('[PopupManager]', '弹窗已加入等待队列', this._queue);
+                return res(false);
             }
 
-            // 动态加载资源
+            // 保存为当前弹窗，阻止新的弹窗请求
+            this._curPopup = { path, options, mode };
+
+            // 先在缓存中查找
+            let node: cc.Node = null;
+            const lastMode: PopupCacheMode = this.modeMap.get(path);
+            switch (lastMode) {
+                // 从预制表中获取
+                case PopupCacheMode.Temporary:
+                    const prefab = this.prefabMap.get(path);
+                    if (cc.isValid(prefab)) {
+                        node = cc.instantiate(prefab);
+                    } else {
+                        this.prefabMap.delete(path);
+                    }
+                    break;
+                // 从节点表中获取
+                case PopupCacheMode.Frequent:
+                    node = this.nodeMap.get(path);
+                    if (!cc.isValid(node)) {
+                        this.nodeMap.delete(path);
+                    }
+                    break;
+            }
+
+            // 缓存中没有，动态加载预制体资源
             if (!cc.isValid(node)) {
-                // 建议在动态加载时添加加载提示并屏蔽用户点击，避免多次点击
-                // 如：PopupManager.loadStartCallback = () => LoadingTip.show();
+                // 建议在动态加载时添加加载提示并屏蔽用户点击，避免多次点击，如下：
+                // PopupManager.loadStartCallback = () => {
+                //     LoadingTip.show();
+                // }
                 this.loadStartCallback && this.loadStartCallback();
+                // 等待加载
                 await new Promise(res => {
                     cc.resources.load(path, (error: Error, prefab: cc.Prefab) => {
                         if (!error) {
-                            node = cc.instantiate(prefab);
-                            this.prefabMap.set(path, prefab);
+                            prefab.addRef();                    // 增加引用计数
+                            node = cc.instantiate(prefab);      // 实例化节点
+                            this.prefabMap.set(path, prefab);   // 保存预制体
+                            this.modeMap.set(path, mode);       // 记录缓存模式
                         }
                         res();
                     });
                 });
-                // 加载完成后隐藏加载提示
-                // 如：PopupManager.loadFinishCallback = () => LoadingTip.hide();
+                // 加载完成后隐藏加载提示，如下：
+                // PopupManager.loadFinishCallback = () => {
+                //     LoadingTip.hide();
+                // }
                 this.loadFinishCallback && this.loadFinishCallback();
             }
 
             // 加载失败（一般是路径错误导致的）
             if (!cc.isValid(node)) {
-                this._curPopup = null;
                 cc.warn('[PopupManager]', '弹窗加载失败', path);
+                this._curPopup = null;
                 return res(false);
             }
 
             // 添加到场景中
             node.setParent(cc.Canvas.instance.node);
+            // 显示在最上层
             node.setSiblingIndex(cc.macro.MAX_ZINDEX);
 
             // 获取继承于 PopupBase 的弹窗组件
@@ -112,28 +130,39 @@ export default class PopupManager {
     /**
      * 展示等待队列中的下一个弹窗
      */
-    public static next() {
-        if (this._curPopup || this._queue.length === 0) return;
+    public static next(): Promise<boolean> {
+        if (this._curPopup || this._queue.length === 0) {
+            return Promise.resolve(false);
+        }
         const request = this._queue.shift();
-        this.show(request.path, request.options, request.mode);
+        return this.show(request.path, request.options, request.mode);
     }
 
     /**
      * 回收弹窗
      * @param path 弹窗路径
      * @param node 弹窗节点
-     * @param mode 回收模式
+     * @param mode 缓存模式
      */
-    private static recycle(path: string, node: cc.Node, mode: PopupRecycleMode) {
+    private static recycle(path: string, node: cc.Node, mode: PopupCacheMode): void {
         switch (mode) {
-            case PopupRecycleMode.OneTime:
+            case PopupCacheMode.Once:
                 node.destroy();
+                if (this.nodeMap.has(path)) {
+                    this.nodeMap.delete(path);
+                }
+                this.release(path);
                 break;
-            case PopupRecycleMode.Temporary:
+            case PopupCacheMode.Temporary:
                 node.destroy();
+                if (this.nodeMap.has(path)) {
+                    this.nodeMap.delete(path);
+                }
                 break;
-            case PopupRecycleMode.Frequent:
-                this.nodeMap.set(path, node);
+            case PopupCacheMode.Frequent:
+                if (!this.nodeMap.has(path)) {
+                    this.nodeMap.set(path, node);
+                }
                 node.removeFromParent(false);
                 break;
         }
@@ -143,29 +172,35 @@ export default class PopupManager {
      * 添加一个弹窗请求到等待队列中，如果当前没有展示中的弹窗则直接展示该弹窗。
      * @param path 弹窗预制体相对路径（如：prefabs/popup）
      * @param options 弹窗选项
-     * @param mode 回收模式
+     * @param mode 缓存模式
      * @param priority 是否优先展示
      */
-    public static push(path: string, options: any = null, mode: PopupRecycleMode = PopupRecycleMode.Temporary, priority: boolean = false) {
+    public static push(path: string, options: any = null, mode: PopupCacheMode = PopupCacheMode.Temporary, priority: boolean = false): Promise<boolean> {
+        // 直接展示
         if (!this._curPopup) {
-            this.show(path, options, mode);
-            return;
+            return this.show(path, options, mode);
         }
-
+        // 加入队列
         if (priority) {
             this._queue.unshift({ path, options, mode });
         } else {
             this._queue.push({ path, options, mode });
         }
+        return Promise.resolve(true);
     }
 
-    // /**
-    //  * 释放弹窗以及资源
-    //  * @param path 弹窗路径
-    //  */
-    // private release(path: string) {
-    //     // TODO
-    // }
+    /**
+     * 尝试释放弹窗资源（注意：弹窗内部动态加载的资源请自行释放）
+     * @param path 弹窗路径
+     */
+    public static release(path: string): void {
+        let prefab = this.prefabMap.get(path);
+        if (prefab) {
+            this.prefabMap.delete(path);
+            prefab.decRef();
+            prefab = null;
+        }
+    }
 
 }
 
@@ -176,14 +211,14 @@ export interface PopupRequest {
     /** 弹窗选项 */
     options: any;
     /** 优化模式 */
-    mode: PopupRecycleMode,
+    mode: PopupCacheMode,
 }
 
-/** 弹窗回收模式 */
-export enum PopupRecycleMode {
+/** 弹窗缓存模式 */
+export enum PopupCacheMode {
     /** 一次性（立即销毁，不保留预制体） */
-    OneTime = 1,
-    /** 偶尔（立即销毁，保留预制体） */
+    Once = 1,
+    /** 短期（立即销毁，保留预制体） */
     Temporary = 2,
     /** 频繁（关闭节点，保留预制体） */
     Frequent = 3
