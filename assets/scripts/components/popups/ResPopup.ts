44207f508c6f4843b6f869a4a21456f716a302e4
陈皮皮 <952157129@qq.com>
2020-10-19 15:40:06 +0800

    update

--- assets/scripts/components/popups/ResPopup.ts
+++ assets/scripts/components/popups/ResPopup.ts
@@ -64,7 +64,7 @@ export default class ResPopup extends PopupBase<ResPopupOptions> {
 }
 
 /** 资源弹窗选项类型 */
-export interface ResPopupOptions {
+export type ResPopupOptions = {
     items: {
         name: string;
         url: string
