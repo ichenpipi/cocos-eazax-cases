9bafa364531543de2301b17c7e4fbab23e6b9e2f
陈皮皮 <952157129@qq.com>
2020-10-19 10:08:31 +0800

    update

--- assets/eazax-ccc/core/Resources.ts
+++ assets/eazax-ccc/core/Resources.ts
@@ -1,12 +1,20 @@
 /** 资源管理器 */
 export default class Resources {
 
-    public static load() {
-
+    public static load(path: string): Promise<any> {
+        return new Promise(res => {
+            cc.resources.load(path, (error: Error, result: any) => {
+                res(error || result);
+            });
+        });
     }
 
     public static release() {
 
+    }
+
+    public static getDepsRecursively(uuid: string): string[] {
+        return cc.assetManager.dependUtil.getDepsRecursively(uuid);
     }
 
 }
