29d9dc1007d64c239c2572b5e5a45d2ead2ba483
陈皮皮 <952157129@qq.com>
2021-01-18 16:18:44 +0800

    update

---
+++ build/web-mobile/src/settings.17483.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/main.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "f1619",
+        resources: "6a0ab",
+        main: "d2c6d"
+    }
+};
