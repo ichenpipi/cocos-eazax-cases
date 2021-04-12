da44735bfc7e8608d3a8b81d6b2e143e3b06eeb0
陈皮皮 <952157129@qq.com>
2021-04-12 21:36:46 +0800

    update

--- assets/test/Test.ts
+++ assets/test/Test.ts
@@ -13,14 +13,13 @@ export default class NewClass extends cc.Component {
     }
 
     update(dt) {
-        this.node.rotation += 1;
+        // this.node.eulerAngles.x += 1;
         // this.node.eulerAngles.y += 1;
         // this.node.eulerAngles.z += 1;
-        this.node._fromEuler();
-        this.node.setLocalDirty(cc.Node._LocalDirtyFlag.ALL_ROTATION);
+        // this.node._fromEuler();
+        // this.node.setLocalDirty(cc.Node._LocalDirtyFlag.ALL_ROTATION);
+
         // const z = this.node.eulerAngles.z + 1;
         // cc.Vec3.set(this.node.eulerAngles, 0, 0, z);
-
-        // cc.Node._LocalDirtyFlag.ALL_ROTATION;
     }
 }
