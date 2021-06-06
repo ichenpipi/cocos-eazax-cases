6125d9f693c5e395dc8da83a63b6677b06f54d6a
陈皮皮 <952157129@qq.com>
2021-06-07 00:28:29 +0800

    update

---
+++ build/web-mobile/src/settings.044db.js
@@ -0,0 +1,59 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "background",
+        "orthographic",
+        "perspective",
+        "after effect",
+        "common ui"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/home.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "3e194",
+        resources: "daa4a",
+        main: "0486a"
+    }
+};
