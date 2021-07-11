9ecb6780aad70e41d73eaba251b9460769cc207f
陈皮皮 <952157129@qq.com>
2021-07-11 18:48:15 +0800

    update

--- assets/test/Test_NodeOrder.ts
+++ assets/test/Test_NodeOrder.ts
@@ -1,10 +1,11 @@
+// @ts-nocheck
 const { ccclass, property, executeInEditMode } = cc._decorator;
 
 @ccclass
 @executeInEditMode
 export default class Test_NodeOrder extends cc.Component {
 
-    @property
+    @property({ displayName: 'siblingIndex' })
     protected get siblingIndex() {
         return this.node.getSiblingIndex();
     }
@@ -12,12 +13,22 @@ export default class Test_NodeOrder extends cc.Component {
         this.node.setSiblingIndex(value);
     }
 
-    @property
+    @property({ displayName: 'zIndex' })
     protected get zIndex() {
         return this.node.zIndex;
     }
     protected set zIndex(value) {
         this.node.zIndex = value;
+    }
+
+    @property({ displayName: '_localZOrder' })
+    protected get localZOrder() {
+        return this.node._localZOrder;
+    }
+
+    @property({ displayName: '_localZOrder (二进制)' })
+    protected get localZOrderBinary() {
+        return this.node._localZOrder.toString(2).padStart(32, 0);
     }
 
 }
