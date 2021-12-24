590276eac31e2c20ce3578fcf3f7c4a79f6a582a
陈皮皮 <952157129@qq.com>
2021-12-24 17:24:50 +0800

    不可告人的提交

--- assets/scripts/common/constants/Constants.ts
+++ assets/scripts/common/constants/Constants.ts
@@ -2,4 +2,6 @@
 export enum SceneName {
     /** 首页 */
     Home = 'home',
+    /** 关于 */
+    About = 'about',
 }
--- assets/scripts/home/Home_UI.ts
+++ assets/scripts/home/Home_UI.ts
@@ -1,17 +1,27 @@
+import EventManager from "../../eazax-ccc/core/EventManager";
+import SceneNavigator from "../../eazax-ccc/core/SceneNavigator";
+import { SceneName } from "../common/constants/Constants";
+import { SWITCH_CASE } from "../common/constants/CustomEvents";
+
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Home_UI extends cc.Component {
 
+    @property(cc.Node)
+    protected aboutBtn: cc.Node = null;
+
+    /**
+     * 生命周期：加载
+     */
     protected onLoad() {
         this.init();
         this.registerEvent();
     }
 
-    protected start() {
-        this.reset();
-    }
-
+    /**
+     * 生命周期：销毁
+     */
     protected onDestroy() {
         this.unregisterEvent();
     }
@@ -20,7 +30,7 @@ export default class Home_UI extends cc.Component {
      * 注册事件
      */
     protected registerEvent() {
-
+        this.aboutBtn.on(cc.Node.EventType.TOUCH_END, this.onAboutBtnClick, this);
     }
 
     /**
@@ -38,10 +48,14 @@ export default class Home_UI extends cc.Component {
     }
 
     /**
-     * 重置
+     * 关于按钮点击回调
      */
-    protected reset() {
-
+    protected onAboutBtnClick() {
+        // 跳转场景
+        SceneNavigator.go('about', false, () => {
+            // 事件
+            EventManager.emit(SWITCH_CASE, SceneName.About);
+        });
     }
 
 }
